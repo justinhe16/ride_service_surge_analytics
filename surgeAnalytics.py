b283ba0fe3c1d7c479c95d5584a5c53e939eefba
Justin He <hejustin@c3.local>
2018-03-25 02:07:27 -0700

    update so access only requires server token and not Auth 2.0

--- surgeAnalytics.py
+++ surgeAnalytics.py
@@ -8,35 +8,22 @@ import json
 # to import my own files that contain secrets (placed in gitignore)
 import tokens
 
-# import uber authenticationo tools
-rom uber_rides.auth import AuthorizationCodeGrant
-
-auth_flow = AuthorizationCodeGrant(
-    <CLIENT_ID>,
-    <SCOPES>,
-    <CLIENT_SECRET>,
-    <REDIRECT_URI>
-)
-auth_url = auth_flow.get_authorization_url()
-
 # authorizes into Uber API Service and begins session
-session = auth_flow.get_session(redirect_url)
+session = Session(server_token=tokens.token)
 client = UberRidesClient(session, sandbox_mode=True)
-credentials = session.oauth2credential
 
 # making a Price Estimate Uber API request, from the University of Southern
 # California to Snowglobe Perspective (Dance Studio)
-response = client.estimate_ride(
+response = client.get_price_estimates(
     start_latitude= 34.02235998929801,
     start_longitude= -118.28516006469727,
     end_latitude= 34.02733062053405,
-    end_longitude= -118.03994178771973,
-    seat_count=2
+    end_longitude= -118.03994178771973
 )
 estimate = response.json.get('prices')
 
 # some (temporary) printing to analyze API call returns
-print json.dumps(estimate,sort_keys=True,indent=4, separators=(',', ': '))
+print(json.dumps(estimate,sort_keys=True,indent=4, separators=(',', ': ')))
 
 for iterator in range(len(estimate)):
-    print unicode(estimate[iterator].get("display_name")) + ', $' + unicode(estimate[iterator].get("low_estimate")) + ', $' + unicode(estimate[iterator].get("high_estimate"))
+    print(unicode(estimate[iterator].get("display_name")) + ', $' + unicode(estimate[iterator].get("low_estimate")) + ', $' + unicode(estimate[iterator].get("high_estimate")))
